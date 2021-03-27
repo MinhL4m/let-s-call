948c24e69707d2eb4e94ab3728ec0d54f51e19e9
Minh Lam <ltm1701@gmail.com>
2021-03-27 11:12:39 -0600

    backend\Add_Socket

---
+++ back-end/controllers/socketController.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Message = mongoose.model("Message");
+const User = mongoose.model("User");
+
+const onConnection = (socket, io) => {
+  console.log("Connected: " + socket.userId);
+  socket.on("disconnect", () => {
+    console.log("Disconnect:" + socket.userId);
+  });
+
+  socket.on("joinRoom", ({ chatroomId }) => {
+    // make socket join chatroomid room
+    socket.join(chatroomId);
+    console.log("A user joined chatroom: " + chatroomId);
+  });
+
+  socket.on("leaveRoom", ({ chatroomId }) => {
+    //make socket leave the chatroomid room
+    socket.leave(chatroomId);
+    console.log("A user left chatroom: " + chatroomId);
+  });
+
+  socket.on("chatroomMessage", async ({ chatroomId, message }) => {
+    if (message.trim().length > 0) {
+      const user = await User.findOne({ _id: socket.userId });
+      const newMessage = new Message({
+        chatroom: chatroomId,
+        user: socket.userId,
+        message,
+      });
+
+      // to all socket in chatroom, emit message
+      io.to(chatroomId).emit("newMessage", {
+        message,
+        name: user.name,
+        userId: socket.userId,
+      });
+
+      await newMessage.save();
+    }
+  });
+};
+
+module.exports = { onConnection };

1c72265875a36cbdec71eee01a00fcd1355d9411
Minh Lam <ltm1701@gmail.com>
2021-03-30 21:50:03 -0600

    frontend\friend add component

---
+++ front-end/src/components/dashboard/FriendAdd.jsx
@@ -0,0 +1,54 @@
+import axios from "axios";
+import { useState, createRef } from "react";
+import { useUserValue } from "../../context/user-provider";
+
+export const FriendAdd = ({ addUser }) => {
+  const [error, setError] = useState("");
+  const idRef = createRef();
+  const { user } = useUserValue();
+
+  const onClick = async () => {
+    const id = idRef.current.value;
+    if (!id) {
+      setError("Id cannot be empty");
+    } else {
+      try {
+        const res = await axios.put("http://localhost:3001/friend/addfriend", {
+          userId: user.id,
+          friendId: id,
+        });
+        res ? addUser(res) : setError("Something went wrong");
+      } catch (e) {
+        if (e.response) {
+          setError(e.response.data.message);
+        } else {
+          setError("Something went wrong");
+        }
+      }
+    }
+  };
+
+  return (
+    <div className="mt-5">
+      <h2 className="text-center ">Add Friend</h2>
+      <div className="TypeAheadDropDown">
+        <input placeholder="Friend Id" className="form-control" ref={idRef} />
+
+        {error && (
+          <small id="passwordHelp" className="text-danger">
+            {error}
+          </small>
+        )}
+        <div className="create-btn d-flex flex-row-reverse ">
+          <input
+            type="button"
+            name="create"
+            className="btn btn-md btn-outline-dark mt-3"
+            value="Add"
+            onClick={onClick}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};

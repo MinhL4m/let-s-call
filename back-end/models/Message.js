9496896767df6e448713c13b9b58be6a51bec7b7
Minh Lam <ltm1701@gmail.com>
2021-03-31 20:42:05 -0600

    backend\refactor Message model

--- back-end/models/Message.js
+++ back-end/models/Message.js
@@ -1,21 +1,30 @@
 const mongoose = require("mongoose");
 
-const chatRoomSchema = mongoose.Schema({
-  chatroom: {
-    // get Id of ref schema
-    type: mongoose.Schema.Types.ObjectId,
-    required: "Chatroom is required!",
-    ref: "Chatroom",
+const chatRoomSchema = mongoose.Schema(
+  {
+    chatroom: {
+      // get Id of ref schema
+      type: mongoose.Schema.Types.ObjectId,
+      required: "Chatroom is required!",
+      ref: "Chatroom",
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: "Chatroom is required!",
+      ref: "User",
+    },
+    message: {
+      type: String,
+      required: "Message is required",
+    },
+    name: {
+      type: String,
+      required: "Name is required",
+    },
   },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: "Chatroom is required!",
-    ref: "User",
-  },
-  message: {
-    type: String,
-    required: "Message is required",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.export = mongoose.model("Message", chatRoomSchema);

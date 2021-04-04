50992c4a25ed2fa4482d8d635f9c265fb1e7ce80
Minh Lam <ltm1701@gmail.com>
2021-04-03 21:56:20 -0600

    frontend\add call button and end call button

--- front-end/src/components/chat/ChatInput.jsx
+++ front-end/src/components/chat/ChatInput.jsx
@@ -1,4 +1,10 @@
-export const ChatInput = ({ messageRef, sendMessage }) => {
+export const ChatInput = ({
+  messageRef,
+  sendMessage,
+  onVideoCall,
+  isEndCall,
+  onEndCall,
+}) => {
   return (
     <form className="chatroom-actions d-flex" onSubmit={sendMessage}>
       <input
@@ -15,6 +21,23 @@ export const ChatInput = ({ messageRef, sendMessage }) => {
         className="btn btn-md btn-outline-dark send-btn"
         value="Send"
       />
+      {isEndCall ? (
+        <input
+          type="button"
+          name="end"
+          className="btn btn-md btn-outline-danger send-btn"
+          value="End Call"
+          onClick={onEndCall}
+        />
+      ) : (
+        <input
+          type="button"
+          name="call"
+          className="btn btn-md btn-outline-dark send-btn"
+          value="Video Call"
+          onClick={onVideoCall}
+        />
+      )}
     </form>
   );
 };

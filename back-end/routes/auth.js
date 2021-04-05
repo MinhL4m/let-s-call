c5498bffaac731b5c4098dc61fcf5b1b2a55527d
Minh Lam <ltm1701@gmail.com>
2021-04-05 09:06:17 -0600

    backend\add routes for resetPassword and requestResetPassword

--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
-const { logIn, signUp, checkLogIn } = require("../controllers/authController");
+const { logIn, signUp, checkLogIn, requestResetPassword, resetPassword } = require("../controllers/authController");
 const { catchErrors } = require("../handlers/errorHandler");
 
 router.post("/login", catchErrors(logIn));
 router.post("/register", catchErrors(signUp));
 router.get("/check-auth", catchErrors(checkLogIn));
+router.post('/request-reset-password', catchErrors(requestResetPassword))
+router.post('/reset-password', catchErrors(resetPassword))
 
 module.exports = router;

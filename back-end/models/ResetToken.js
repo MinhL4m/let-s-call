8f7163d3a61dbf41235b5f42a86bab8af03604c2
Minh Lam <ltm1701@gmail.com>
2021-04-05 12:26:25 -0600

    backend\create TTL index for mongo db

--- back-end/models/ResetToken.js
+++ back-end/models/ResetToken.js
@@ -1,20 +1,19 @@
-
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const resetTokenSchema = mongoose.Schema({
-    user:{
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref:"user"
-    },
-    token:{
-        type: String,
-        required: true,
-    },
-    createdAt:{
-        type: Date,
-        default: Date.now,
-        expires: 7200, // auto delete in 1 min 
-    }
-})
-
-module.exports = mongoose.model("ResetToken", resetTokenSchema)
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "user",
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 7200,
+  },
+});
+resetTokenSchema.index({ "createdAt": 1 }, { expireAfterSeconds: 7200 });
+module.exports = mongoose.model("ResetToken", resetTokenSchema);

7c30343e5515c7ed9b0556a9ab1c820fae6da478
Minh Lam <ltm1701@gmail.com>
2021-04-06 08:03:30 -0600

    frontend\add onChat function for button

--- front-end/src/pages/FriendPage.jsx
+++ front-end/src/pages/FriendPage.jsx
@@ -1,15 +1,15 @@
 import { useState, useEffect } from "react";
 import { useUserValue } from "../context/user-provider";
-import { Link } from "react-router-dom";
+import { useHistory, withRouter } from "react-router-dom";
 import axios from "axios";
 import { FriendAdd } from "../components/dashboard/FriendAdd";
-export function FriendPage({ socket }) {
+function FriendPage() {
   const [friends, setFriends] = useState([]);
   const { user } = useUserValue();
+  const history = useHistory();
 
   useEffect(() => {
     async function getChatroom() {
-      console.log(user);
       try {
         let res = await axios.get("http://localhost:3001/friend", {
           params: { userId: user.id },
@@ -21,6 +21,21 @@ export function FriendPage({ socket }) {
 
     getChatroom();
   }, []);
+
+  const onChat = async (friendName, friendId) => {
+    try {
+      const res = await axios.post("http://localhost:3001/chatroom", {
+        userId: user.id,
+        friendId,
+        userName: user.name,
+        friendName,
+      });
+
+      history.push(`/chat/${res.data.room._id}`);
+    } catch (e) {
+      //TODO Modal pop up
+    }
+  };
 
   return (
     <div className="container">
@@ -35,13 +50,21 @@ export function FriendPage({ socket }) {
               className="list-group-item d-flex d-flex justify-content-between"
             >
               <div>{friend.name}</div>
-              <Link to={"/chat/" + friend._id}>
-                <div className="join">Join</div>
-              </Link>
+              <input
+                type="button"
+                name="chat"
+                className="btn btn-md btn-outline-dark"
+                value="Chat"
+                onClick={() => onChat(friend.name, friend._id)}
+              />
             </li>
           ))}
       </ul>
-      {friends.length === 0 && <p class="text-center mt-3">Not friend found</p>}
+      {friends.length === 0 && (
+        <p className="text-center mt-3">Not friend found</p>
+      )}
     </div>
   );
 }
+
+export default withRouter(FriendPage);
